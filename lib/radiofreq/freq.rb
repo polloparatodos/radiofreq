c408dd8401b739efac40f1c94602ee266643bf62
Ryan Sherer <ryan.m.sherer@gmail.com>
2020-07-19 02:33:38 -0500

    Updated lib/radiofreq/freq.rb

--- lib/radiofreq/freq.rb
+++ lib/radiofreq/freq.rb
@@ -1,10 +1,54 @@
 module Radiofreq
   class Freq
-    def self.portray(freq)
-      if freq.between?(30,300)
-        "VHF"
-      elsif freq.between?(300,3000)
-        "UHF"
+    def self.portray(freq, unit)
+      case unit
+        when 'Hz'
+          case freq
+            when freq < 3
+              "Tremendously Low Frequency (TLF)"
+            when 3..30
+              "Extremely Low Frequency (ELF)"
+            when 30.001..300
+              "Super Low Frequency (SLF)"
+            when 300.001..3000
+              "Ultra Low Frequency (ULF)"
+            else
+              "Invalid frequency provided: #{freq + unit}"
+            end
+        when 'kHz'
+          case freq
+            when 3..30
+              "Very Low Frequency VLF)"
+            when 30.001..300
+              "Low Frequency (LF)"
+            when 300.001..3000
+              "Medium Frequency (MF)"
+            else
+              "Invalid frequency provided: #{freq + unit}"
+            end
+        when 'MHz'
+          case freq
+            when 3..30
+              "High Frequency (HF)"
+            when 30.001..300
+              "Very High Frequency (VHF)"
+            when 300.001..3000
+              "Ultra High Frequency (UHF)"
+            else
+              "Invalid frequency provided: #{freq + unit}"
+            end
+        when 'GHz'
+          case freq
+            when 3..30
+              "Super High Frequency (SHF)"
+            when 30.001..300
+              "Extremely High Frequency (EHF)"
+            when 300.001..3000
+              "Tremendously High Frequency (THF)"
+            else
+              "Invalid frequency provided: #{freq + unit}"
+            end
+        end
       end
     end
   end

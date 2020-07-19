a10ec493d7359fec46e35a119ec0c86eefc1f0c9
Ryan Sherer <ryan.m.sherer@gmail.com>
2020-07-19 02:54:41 -0500

    Updated spec/radiofreq_spec.rb

--- spec/radiofreq_spec.rb
+++ spec/radiofreq_spec.rb
@@ -12,6 +12,10 @@ describe Radiofreq::Freq do
   it "Ultra High Frequency (UHV)" do
     expect(Radiofreq::Freq.portray(446.000, 'MHz')).to include("UHF")
   end
+  
+  it "Out of bounds" do
+    expect(Radiofreq::Freq.portray(3001, 'GHz')).to start_with("Invalid frequency provided:")
+  end
 
   it "Invalid Frequency" do
     expect(Radiofreq::Freq.portray(3, 'Invalid')).to start_with("Invalid frequency unit:")

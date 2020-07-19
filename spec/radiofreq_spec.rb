b6bcfd17600856cc5382caa45dd76b1d34bf2a20
Ryan Sherer <ryan.m.sherer@gmail.com>
2020-07-19 02:35:51 -0500

    Updated spec/radiofreq_spec.rb

--- spec/radiofreq_spec.rb
+++ spec/radiofreq_spec.rb
@@ -2,11 +2,10 @@ require 'radiofreq'
 
 describe Radiofreq::Freq do
   it "Very High Frequency (VHF)" do
-    expect(Radiofreq::Freq.portray(146.520)).to include("VHF")
+    expect(Radiofreq::Freq.portray(146.520, 'MHz')).to include("VHF")
   end
 
   it "Ultra High Frequency (UHV)" do
-    expect(Radiofreq::Freq.portray(446.000)).to include("UHF")
+    expect(Radiofreq::Freq.portray(446.000, 'MHz')).to include("UHF")
   end
 end
-

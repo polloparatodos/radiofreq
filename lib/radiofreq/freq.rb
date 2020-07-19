42e34895d5523d1b22c3fc60a81e230a20fad206
Ryan Sherer <ryan.m.sherer@gmail.com>
2020-07-19 02:02:52 -0500

    Updated lib/radiofreq/freq.rb

--- lib/radiofreq/freq.rb
+++ lib/radiofreq/freq.rb
@@ -1,9 +1,9 @@
 module Radiofreq
   class Freq
-    def self.portray(freq:Float)
+    def self.portray(freq)
       if freq.between?(30,300)
         "VHF"
-      elsif freq.between(300,3000)
+      elsif freq.between?(300,3000)
         "UHF"
       end
     end
